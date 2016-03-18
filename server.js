0bb84621cf85b6a8d412b98596f8e52cd79329b3
ankihg <hagelin.annika@gmail.com>
2016-03-18 14:16:23 -0700

    simple sever setup

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ let app = express();
 let router = new express.Router();
 
 let models = require(__dirname + '/models');
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} request for ${req.url}`);
+  next();
+});
+
+
+
+
+app.listen(3000, () => console.log('server speaking.'));
